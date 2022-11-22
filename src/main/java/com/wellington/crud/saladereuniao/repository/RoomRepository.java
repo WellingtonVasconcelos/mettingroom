9437cdd737e2f3b46556e03b13fe0a634ff5a7c2
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-11-22 17:00:43 -0300

    Room and Repository - Done

---
+++ src/main/java/com/wellington/crud/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.wellington.crud.saladereuniao.repository;
+
+import com.wellington.crud.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
